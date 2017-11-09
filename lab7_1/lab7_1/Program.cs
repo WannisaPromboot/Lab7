e3aa73ce6243b29850a2aaba4c079d56477947e7
Wannamiu <58030218@kmitl.ac.th>
2017-11-09 10:15:03 +0700

    commited by wannisa

---
+++ lab7_1/lab7_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            data[1] = 0;
+            data[2] = 1;
+            data[3] = 9;
+            data[8] = 10;
+            
+           
+        Console.WriteLine("tatal = "+data[0]+ data[1] + data[2] + data[3] + data[4] + data[5] +
+            data[6] + data[7] + data[8] + data[9]);
+            
+        }
+    }
+}
